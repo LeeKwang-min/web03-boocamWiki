aa894f9f623d99a87663b1c8588d709e7d41f7a6
Jeong Yo Han <ingyeoking13@gmail.com>
2021-11-11 18:11:59 +0900

    더안좋음

--- client/src/components/WikiContentsIndex.jsx
+++ client/src/components/WikiContentsIndex.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable prefer-destructuring */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable prefer-destructuring */
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
@@ -33,72 +33,85 @@ const Padd = styled.div`
   padding-left: 20px;
 `;
 
+const Obj = {
+  headerCount: 1,
+  header2Count: 1,
+  header3Count: 1,
+  reset: () => {
+    Obj.headerCount = 1;
+    Obj.header2Count = 1;
+    Obj.header3Count = 1;
+  },
+  h1: ({ node, ...props }) => {
+    Obj.headerCount += 1;
+    Obj.header2Count = 1;
+    Obj.header3Count = 1;
+    let text;
+    if (props.children) {
+      text = props.children[0];
+    }
+    const hh = `#${text}`;
+    return (
+      <li style={{ listStyle: 'none' }}>
+        <a href={hh} style={{ display: 'inline' }}>
+          {Math.round(Obj.headerCount / 2 - 1)}
+        </a>{' '}
+        <h2 {...props} style={{ display: 'inline' }} />
+      </li>
+    );
+  },
+  h2: ({ node, ...props }) => {
+    console.log(node, props);
+    Obj.header2Count += 1;
+    let text;
+    if (props.children) {
+      text = props.children[0];
+    }
+    const hh = `#${text}`;
+    return (
+      <li style={{ listStyle: 'none', paddingLeft: '20px' }}>
+        <a href={hh} style={{ display: 'inline' }}>
+          {Math.round(Obj.headerCount / 2 - 1)}.{Math.round(Obj.header2Count / 2 - 1)}
+        </a>{' '}
+        <h3 {...props} style={{ display: 'inline' }} />
+      </li>
+    );
+  },
+  h3: ({ node, ...props }) => {
+    console.log(node, props);
+    Obj.header3Count += 1;
+    let text;
+    if (props.children) {
+      text = props.children[0];
+    }
+    const hh = `#${text}`;
+    return (
+      <li style={{ listStyle: 'none', paddingLeft: '40px' }}>
+        <a href={hh} style={{ display: 'inline' }}>
+          {Math.round(Obj.headerCount / 2 - 1)}.{Math.round(Obj.header2Count / 2 - 1)}.
+          {Math.round(Obj.header3Count / 2 - 1)}
+        </a>{' '}
+        <h4 {...props} style={{ display: 'inline' }} />
+      </li>
+    );
+  },
+};
+
 const WikiContentsIndex = ({ text, title }) => {
-  let [headerCount] = useState(1);
-  let [header2Count] = useState(1);
-  let [header3Count] = useState(1);
   return (
     <Index>
       <Title> {title}</Title>
       <Padd>
         <ReactMarkdown
-          allowedElements={['h1', 'h2', 'h3']}
-          components={{
-            h1: ({ node, ...props }) => {
-              headerCount += 1;
-              header2Count = 1;
-              header3Count = 1;
-              let text;
-              if (props.children) {
-                text = props.children[0];
-              }
-              const hh = `#${text}`;
-              return (
-                <li style={{ listStyle: 'none' }}>
-                  <a href={hh} style={{ display: 'inline' }}>
-                    {Math.round(headerCount / 2 - 1)}
-                  </a>{' '}
-                  <h2 {...props} style={{ display: 'inline' }} />
-                </li>
-              );
-            },
-            h2: ({ node, ...props }) => {
-              console.log(node, props);
-              header2Count += 1;
-              let text;
-              if (props.children) {
-                text = props.children[0];
-              }
-              const hh = `#${text}`;
-              return (
-                <li style={{ listStyle: 'none', paddingLeft: '20px' }}>
-                  <a href={hh} style={{ display: 'inline' }}>
-                    {Math.round(headerCount / 2 - 1)}.{Math.round(header2Count / 2 - 1)}
-                  </a>{' '}
-                  <h3 {...props} style={{ display: 'inline' }} />
-                </li>
-              );
-            },
-            h3: ({ node, ...props }) => {
-              console.log(node, props);
-              header3Count += 1;
-              let text;
-              if (props.children) {
-                text = props.children[0];
-              }
-              const hh = `#${text}`;
-              return (
-                <li style={{ listStyle: 'none', paddingLeft: '40px' }}>
-                  <a href={hh} style={{ display: 'inline' }}>
-                    {Math.round(headerCount / 2 - 1)}.{Math.round(header2Count / 2 - 1)}.
-                    {Math.round(header3Count / 2 - 1)}
-                  </a>{' '}
-                  <h4 {...props} style={{ display: 'inline' }} />
-                </li>
-              );
-            },
+          allowElement={(elem, index, parent) => {
+            console.log(elem, parent);
+            if (elem.tagName === 'h1') return true;
+            if (parent.tagName === 'h1') return true;
+            return false;
           }}
           includeElementIndex="true"
+          unwrapDisallowed="true"
+          components={Obj}
         >
           {text}
         </ReactMarkdown>
